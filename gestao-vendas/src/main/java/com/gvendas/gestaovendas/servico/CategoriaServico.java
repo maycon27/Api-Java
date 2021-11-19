a066b18156caa5b15e755bf30f3ab383f0aaa855
Maycon Douglas <mayconpalmeiras2013@hotmail.com>
2021-11-19 13:56:43 -0300

    interação com o banco

---
+++ gestao-vendas/src/main/java/com/gvendas/gestaovendas/servico/CategoriaServico.java
@@ -0,0 +1,29 @@
+package com.gvendas.gestaovendas.servico;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gvendas.gestaovendas.entidades.Categoria;
+import com.gvendas.gestaovendas.repositorio.CategoriaRepositorio;
+
+@Service
+public class CategoriaServico {
+
+	@Autowired
+	private CategoriaRepositorio categoriaRepositorio;
+	
+	public List<Categoria> listarTodas(){
+		return categoriaRepositorio.findAll();
+	}
+	
+	public Optional<Categoria> buscarPorId(Long codigo){
+		return categoriaRepositorio.findById(codigo);
+	}
+	
+	public Categoria salvar (Categoria categoria) {
+		return categoriaRepositorio.save(categoria);
+	}
+}
