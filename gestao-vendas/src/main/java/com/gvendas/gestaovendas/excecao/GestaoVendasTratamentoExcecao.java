a51a6a9ee0094eeab0cbdf61f2285bf4a448b6e6
Maycon Douglas <mayconpalmeiras2013@hotmail.com>
2021-11-22 09:10:00 -0300

    tratamento de erros

---
+++ gestao-vendas/src/main/java/com/gvendas/gestaovendas/excecao/GestaoVendasTratamentoExcecao.java
@@ -0,0 +1,52 @@
+package com.gvendas.gestaovendas.excecao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice 
+public class GestaoVendasTratamentoExcecao extends ResponseEntityExceptionHandler {
+	
+	private static final String CONSTANT_VALIDATION_NOT_BLANK = "NotBlank";
+	private static final String CONSTANT_VALIDATION_LENGTH = "Length";
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Erro> erros = gerarListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	private List<Erro> gerarListaDeErros(BindingResult bindingResult) {
+		List<Erro> erros = new ArrayList<Erro>();
+		bindingResult.getFieldErrors().forEach(FieldError -> {
+			String msgUsuario = tratarMensagemDeErroParaUsuario(FieldError);
+			String msgDesevolvedor = FieldError.toString();
+			
+			erros.add(new Erro(msgUsuario,msgDesevolvedor));
+		});
+		
+		return erros;
+	}
+
+	private String tratarMensagemDeErroParaUsuario(FieldError fieldError) {
+		if(fieldError.getCode().equals(CONSTANT_VALIDATION_NOT_BLANK)) {
+			return fieldError.getDefaultMessage().concat(" é obrigatório.");
+		}
+		if(fieldError.getCode().equals(CONSTANT_VALIDATION_LENGTH)) {
+			return fieldError.getDefaultMessage().concat(String.format(" deve ter entre %s e %s caracteres", fieldError.getArguments()[2], 
+					fieldError.getArguments()[1]));
+		}
+		return fieldError.toString();
+	}
+
+}
